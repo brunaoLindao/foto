da7c2ff3aad6dd027b3e6c12e624e89b6d9099df
brunaoLindao <116676898+brunaoLindao@users.noreply.github.com>
2023-06-21 14:37:30 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,43 @@
+var SpeechRecognition = window.webkitSpeechRecognition;
+var recognition = new SpeechRecognition();
+var camera = document.getElementById('camera');
+function start() {
+    document.getElementById('textbox').innerHTML = '';
+    recognition.start();
+}
+function speak() {
+    var fala = window.speechSynthesis;
+    conteudo = 'tirando sua selfie em 5 segundos';
+    var converter = new SpeechSynthesisUtterance(conteudo);
+    fala.speak(converter);
+    Webcam.attach(camera);
+}
+recognition.onresult = function (event) {
+    console.log(event);
+    var texto = event.results[0][0].transcript;
+    document.getElementById('textbox').innerHTML = texto;
+    if (texto == 'tire minha selfie') {
+        speak();
+        setTimeout(function () {
+            takeSelfie();
+            Save();
+        });
+    }
+
+}
+Webcam.set({
+    width: 300,
+    height: 300,
+    image_format: 'jpeg',
+    jpeg_quality: 90
+});
+function takeSelfie() {
+    Webcam.snap(function (data_uri) {
+        document.getElementById('result').innerHTML = '<img id = "selfie" src= "' + data_uri + '"/>';
+    });
+}
+function Save(){
+    imagem = document.getElementById('selfie').src;
+    document.getElementById('link').href = imagem;
+    document.getElementById('link').click();
+}
